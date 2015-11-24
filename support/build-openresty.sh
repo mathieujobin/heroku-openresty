cd5ff9a64eaaa45f3bad8ba0d9a8f57fcafa04b4
Mathieu Jobin <mathieu@justbudget.com>
2015-11-24 15:33:06 -0500

    adding 2 rockspec for 1.9.3

--- support/build-openresty.sh
+++ support/build-openresty.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 base_dir=$(pwd)
-openresty_tar="ngx_openresty-1.7.10.1.tar.gz"
+openresty_tar="ngx_openresty-1.9.3.1.tar.gz"
 pcre_tar="pcre-8.37.tar.gz"
 
 echo "$(tput setaf 2)Downloading openresty...$(tput sgr0)"
